a7fecd49b05105943ca3c8120e8642183b5d238a
Al-un <alun.sng@gmail.com>
2018-12-14 20:15:09 +0100

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,20 @@
 /**
- * SW has not access to DOM
- * @param {*} msg 
- */
-const log = (msg) => {
-  console.log(msg);
-};
-
-/**
  * Installing SW
  */
 self.addEventListener('install', event => {
-  log(event);
+  console.log(event);
 });
 
 /**
  * Activating SW
  */
 self.addEventListener('activate', event => {
-  log(event);
+  console.log(event);
 });
 
 /**
  * Intercept request
  */
 self.addEventListener('fetch', event => {
-  log(event);
+  console.log(event);
 });
