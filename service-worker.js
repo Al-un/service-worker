f36689912ea7a575faba14c3a6b9cf29ca8ae509
Al-un <alun.sng@gmail.com>
2018-12-14 20:32:04 +0100

    Update

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,21 @@ function log(obj) {
   console.log(obj);
 }
 
+const CACHE_VERSION = 'sw-v1';
+const CACHE_FILES = ['/', 'img/miku.png', 'index.js', 'main.css'];
+
 /**
  * Installing SW
  */
 self.addEventListener('install', event => {
   log('[SW] installing');
   log(event);
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(cache => {
+      log(`Opening cache version ${CACHE_VERSION}`);
+      return cache.addAll(CACHE_FILES);
+    })
+  );
 });
 
 /**
